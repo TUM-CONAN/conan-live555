a3086eaf239da6ac01aabf69d655fbff1f60242f
julien.waechter <julien.waechter@gmail.com>
2019-01-29 19:05:06 +0100

    feat(live555): add live555 conan package 1.21.0

---
+++ conanfile.py
@@ -0,0 +1,39 @@
+from conans import ConanFile, tools, CMake
+from conans.util import files
+import os
+import shutil
+
+class Live555Conan(ConanFile):
+    name = "live555"
+    version = "1.21.0"
+    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"shared": [True, False]}
+    default_options = "shared=True"
+    url = "https://git.ircad.fr/conan/conan-live555"
+    license = "GNU LGPL"
+    exports = [
+        "patches/CMakeProjectWrapper.txt"
+    ]
+    description = "Multimedia streaming library, using open standard protocols (RTP/RTCP, RTSP, SIP)"
+    source_subfolder = "source_subfolder"
+    build_subfolder = "build_subfolder"
+
+    def configure(self):
+        del self.settings.compiler.libcxx
+
+    def source(self):
+        tools.get("https://github.com/MobotixAG/live666/archive/releases/{0}.tar.gz".format(self.version))
+        os.rename("live666-releases-" + self.version, self.source_subfolder)
+
+    def build(self):
+        shutil.move("patches/CMakeProjectWrapper.txt", "CMakeLists.txt")
+        cmake = CMake(self)
+        cmake.configure(build_folder=self.build_subfolder)
+        if not tools.os_info.is_windows:
+            cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = "ON"
+        cmake.build()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
