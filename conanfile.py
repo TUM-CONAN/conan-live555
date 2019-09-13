feca5e4c2cc32bc011dcd78f273fae5706074c87
julien.waechter <julien.waechter@gmail.com>
2019-09-13 12:32:15 +0200

    feat(gitlab-ci): use gitlab-ci conan template files

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 from conans import ConanFile, tools, CMake
-from conans.util import files
 import os
 import shutil
+
 
 class Live555Conan(ConanFile):
     name = "live555"
@@ -37,11 +37,11 @@ class Live555Conan(ConanFile):
 
         shutil.move("patches/CMakeProjectWrapper.txt", "CMakeLists.txt")
         cmake = CMake(self)
-        
+
         # Set common flags
         cmake.definitions["SIGHT_CMAKE_C_FLAGS"] = common.get_c_flags()
         cmake.definitions["SIGHT_CMAKE_CXX_FLAGS"] = common.get_cxx_flags()
-        
+
         cmake.configure(build_folder=self.build_subfolder)
         if not tools.os_info.is_windows:
             cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = "ON"
