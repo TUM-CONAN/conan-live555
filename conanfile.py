6d35a69b5f30b8794040b6b4aa56bd5f443f2aad
Ulrich Eck <ulrich.eck@tum.de>
2023-04-25 17:11:09 +0200

    new conan2 package: live555

--- conanfile.py
+++ conanfile.py
@@ -1,59 +1,79 @@
-from conans import ConanFile, tools, CMake
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout, CMakeDeps
+from conan.tools.scm import Git
+from conan.tools.files import load, update_conandata, copy, replace_in_file, collect_libs, patch
 import os
 
 
 class Live555Conan(ConanFile):
+
     name = "live555"
     package_revision = ""
     upstream_version = "2022.01.11"
     version = "{0}{1}".format(upstream_version, package_revision)
-    generators = "cmake"
-    settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False]}
-    default_options = "shared=True"
+
+    description = "Multimedia streaming library, using open standard protocols (RTP/RTCP, RTSP, SIP)"
     url = "https://github.com/TUM-CONAN/conan-live555"
     license = "GNU LGPL"
-    exports = ["patches/*",]
-    description = "Multimedia streaming library, using open standard protocols (RTP/RTCP, RTSP, SIP)"
-    source_subfolder = "source_subfolder"
-    build_subfolder = "build_subfolder"
 
-    def requirements(self):
-        self.requires("ircad_common/1.0.2@camposs/stable")
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+    }
+    default_options = {
+        "shared": True,
+    }
+
+    exports = ["patches/*",]
+
+    def export(self):
+        update_conandata(self, {"sources": {
+            "commit": "{}".format(self.version),
+            "url": "https://github.com/TUM-CAMP-NARVIS/live666.git"
+        }})
 
     def source(self):
-        tools.get("https://github.com/TUM-CAMP-NARVIS/live666/archive/refs/tags/{}.zip".format(self.upstream_version))
-        os.rename("live666-" + self.upstream_version, self.source_subfolder)
+        git = Git(self)
+        sources = self.conan_data["sources"]
+        git.clone(url=sources["url"], target=self.source_folder)
+        git.checkout(commit=sources["commit"])
 
-    def _cmake_configure(self):
-        # Import common flags and defines
-        import common
-        tools.patch(base_path=self.source_subfolder, patch_file='patches/increase_bank_size.patch')
+        patch(self, base_path=self.source_folder,
+              patch_file=os.path.join(self.recipe_folder, "patches", "increase_bank_size.patch"))
 
-        # Generate Cmake wrapper
-        common.generate_cmake_wrapper(
-            cmakelists_path='CMakeLists.txt',
-            source_subfolder=self.source_subfolder,
-            build_type=self.settings.build_type
-        )
+    def generate(self):
+        tc = CMakeToolchain(self)
 
-        cmake = CMake(self)
+        def add_cmake_option(option, value):
+            var_name = "{}".format(option).upper()
+            value_str = "{}".format(value)
+            var_value = "ON" if value_str == 'True' else "OFF" if value_str == 'False' else value_str
+            tc.variables[var_name] = var_value
 
-        if not tools.os_info.is_windows:
-            cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = "ON"
+        for option, value in self.options.items():
+            add_cmake_option(option, value)
 
-        cmake.configure()
-        return cmake
+        tc.cache_variables["CMAKE_POSITION_INDEPENDENT_CODE"] = "ON"
 
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
+
+    def layout(self):
+        cmake_layout(self, src_folder="source_folder")
 
     def build(self):
-        cmake = self._cmake_configure()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        cmake = self._cmake_configure()
+        cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
-        self.cpp_info.includedirs = ['include', 'include/live555/UsageEnvironment', 'include/live555/BasicUsageEnvironment', 'include/live555/groupsock', 'include/live555/liveMedia']
+        self.cpp_info.libs = collect_libs(self)
+        self.cpp_info.includedirs = ['include',
+                                     'include/live555/UsageEnvironment',
+                                     'include/live555/BasicUsageEnvironment', 'include/live555/groupsock', 'include/live555/liveMedia']
